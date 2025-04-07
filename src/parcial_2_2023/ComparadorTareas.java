bc8440d07ee7f7d02a4838a32ba8300f4a707997
Arturo <arlocast77@gmail.com>
2025-04-07 11:55:50 +0200

    Corrección de la clase del comparador

--- src/parcial_2_2023/ComparadorTareas.java
+++ src/parcial_2_2023/ComparadorTareas.java
@@ -5,9 +5,15 @@ import java.util.Comparator;
 public class ComparadorTareas implements Comparator<Tarea> {
 	
 	@Override
-	public int compare(Tarea t1, Tarea t2) {
-		if(!t1.tienePlazo() && !t2.tienePlazo()) {
+	public int compare(Tarea t1, Tarea t2) { // -1: < 0: = 1: >
+		if(!t1.tienePlazo() && t2.tienePlazo()) { // 1º null, osea es mayor
 			return 1;
+		}
+		else if (t1.tienePlazo() && !t2.tienePlazo()) { // 2º null, osea es mayor
+			return -1;
+		}
+		else if (!t1.tienePlazo() && !t2.tienePlazo()) { // ambas null, iguales
+			return 0;
 		}
 		return t1.plazo().compareTo(t2.plazo());
 	}
